f1b788aaa057b6f9aa8d0f04bf2e3ebc23607aff
alireza0mohammadi <mr.afg00@gmail.com>
2025-04-21 21:39:55 +0330

    creating a react app and adding tailwind with vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  return (
+    <div className="bg-amber-200">
+      <ListGroup></ListGroup>
+    </div>
+  );
+}
+
+export default App;
